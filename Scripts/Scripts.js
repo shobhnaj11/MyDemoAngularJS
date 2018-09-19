7a88107f31c81cd750beff9d80299c0ca83a4db5
shobhna <shobhna.jayaraman@orange.com>
2018-09-19 22:23:11 +0530

    files html and js

---
+++ Scripts/Scripts.js
@@ -0,0 +1,23 @@
+/// reference path="angular.min.js"/>
+
+var app = angular
+                .module("myModule", [])
+                .controller("myController", function($scope) {
+                    var technologies = [];
+                    technologies = [
+                        { name: "C++", likes: 0, dislikes: 0 },
+                        { name: "Java", likes: 0, dislikes: 0 },
+                        { name: "Python", likes: 0, dislikes: 0 },
+                        { name: "AngularJS", likes: 0, dislikes: 0 },                       
+                    ];
+                    $scope.technologies = technologies;
+
+                    $scope.incrementLikes -function(technology){
+                        technology.likes++;
+                    }
+
+                    $scope.incrementDislikes -function(technology){
+                        technology.dislikes++;
+                    }
+
+                });
